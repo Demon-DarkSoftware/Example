b27969602e75fce716cb47fa00c8c8715167a552
Demon-DarkSoftware <44945453+Demon-DarkSoftware@users.noreply.github.com>
2019-02-02 00:26:55 -0900

    Create Module1.cs

---
+++ Module1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace FFS {
+  class Cyka {
+     public bool isBlyat { get; set; }
+     public bool rotEbal { get; set; }
+     public bool American { get; set; }
+     public bool Noob { get; set; }
+     public Cyka() {}
+  }
+}
